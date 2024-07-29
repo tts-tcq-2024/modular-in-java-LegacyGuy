ede065f0c1f74c14d21c71e27f31049d44fe7e19
LegacyGuy <prathameshkashid26@gmail.com>
2024-07-29 23:06:01 +0530

    Update ColorCodeMainRunTest.java

--- colorcoder/ColorCodeMainRunTest.java
+++ colorcoder/ColorCodeMainRunTest.java
@@ -3,7 +3,7 @@ package colorcoder;
 /**
  * Main class to run the color coding tests and print the reference manual.
  */
-public class ColorCodeMainProgram {
+public class Main {
     public static void main(String[] args) {
         ColorCodeTestSuite.runTests();
         ColorCodeUtilityFunctions.printColorCodingReference();
