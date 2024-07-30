fd697717116a9f80068115f7eb660af3b71a02e0
LegacyGuy <prathameshkashid26@gmail.com>
2024-07-30 12:32:22 +0530

    Main.java

---
+++ colorcoder/Main.java
@@ -0,0 +1,13 @@
+package colorcoder;
+
+public class Main {
+    public static void main(String[] args) {
+        ColorCoderTest.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
+        ColorCoderTest.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
+
+        ColorCoderTest.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
+        ColorCoderTest.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+
+        ColorUtils.printReferenceManual();
+    }
+}
